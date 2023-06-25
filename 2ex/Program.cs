d20152e5b95538feb63fccb2e1c0ff9b43344d42
Nastya <treninanst@gmail.com>
2023-06-25 18:16:23 +0400

    Added 3

---
+++ 2ex/Program.cs
@@ -0,0 +1,44 @@
+﻿//  Задайте одномерный массив, заполненный
+// случайными числами. Найдите сумму элементов, стоящих
+// на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int Prompt (string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+int size = Prompt("Введите размер массива: ");
+int[] array = GetArray(size);
+PrintArray(array);
+Console.WriteLine();
+FindSum(array);
+
+int[] GetArray(int size)
+
+{
+    int[] result = new int[size];
+    for (int i = 0; i < result.Length; i ++)
+    {
+        result[i] = new Random().Next(-10,50);
+    }
+    return result;
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i ++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+void FindSum(int []array)
+{
+    int Sum = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        Sum += array[i];
+    } 
+Console.WriteLine(Sum);
+}
---
+++ 3ex/Program.cs
@@ -0,0 +1,65 @@
+﻿//  Задайте массив вещественных чисел. Найдите
+// разницу между максимальным и минимальным
+// элементов массива.
+// [3 7 22 2 78] -> 76
+
+int Prompt (string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+int size = Prompt("Введите размер массива: ");
+int[] array = GetArray(size);
+PrintArray(array);
+Console.WriteLine();
+int result = FindSum(array);
+PrintSum(result);
+
+int[] GetArray(int size)
+
+{
+    int[] result = new int[size];
+    for (int i = 0; i < result.Length; i ++)
+    {
+        result[i] = new Random().Next(0,50);
+    }
+    return result;
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i ++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+int FindSum (int[] array)
+{
+    int i = 0;
+    int min = array[i];
+    int max = 0;
+    int sum = 0;
+    while (i < array.Length)
+    {
+        if ( array[i] < min)
+        {
+            min = array[i];
+            i ++;
+        }
+        else if (array[i] > max)
+        {
+            max = array[i];
+            i ++;
+        }
+        else
+        {
+            i ++;
+        }
+    }
+    sum = max - min;
+    return sum;
+}
+void PrintSum(int sum)
+{
+    Console.Write(sum);
+}
